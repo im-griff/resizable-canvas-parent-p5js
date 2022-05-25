07f0b32c33fdc89fc0a91e8913f1c3d14daf6dbd
Griffin <gbengraff@me.com>
2022-05-25 17:45:05 -0400

    removed bloat
    Making this more user-friendly to download and use in an existing project

--- sketch.js
+++ sketch.js
@@ -1,36 +1,21 @@
-var divWidth, divHeight;
-var sketchCiner;
+var sketchContainer, divWidth, divHeight;
 var canvas;
-
-var moon;
 
 function setup() {
   sketchContainer = document.getElementById('sketchContainer');
   canvas = createCanvas(1,1);
   canvas.parent(sketchContainer);
   updateCanvasSize();
-  
-  moon = new Moon();
-  
-  background(220);
-  frameRate(60);
-
 }
 
 function draw() {
   checkResize();
   background(0);
-
-  // circle and moon:
-  fill(255); noStroke();
-  circle(width/2, height/2, 80)
-  moon.show();
 }
 
 function checkResize() {
   // If the width or height is different, run updateCanvasSize()
-  if ((sketchContainer.offsetWidth  != divWidth) ||
-       sketchContainer.offsetHeight != divHeight) {
+  if ((sketchContainer.offsetWidth  != divWidth) || sketchContainer.offsetHeight != divHeight) {
     updateCanvasSize();
   }
 }
@@ -44,5 +29,4 @@ function updateCanvasSize() {
   
   // debug
   console.log('Sketch resized to: ' + divWidth + ' ' + divHeight);
-  
 }
